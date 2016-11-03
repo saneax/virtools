8bd3f5fe468cb5c22badf7af98dc978c70a8d9d7
Sanjay Upadhyay <supadhya@redhat.com>
2016-11-03 18:12:38 +0530

    adding gitignore

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name = 'virtools',
+      packages = ['virtools'], # this must be the same as the name above
+      version = '0.1',
+      description = 'collection of libvirt helper scripts',
+      author = 'Sanjay Upadhyay',
+      author_email = 'saneax@gmail.com',
+      url = 'https://github.com/saneax/virtools', # use the URL to the github repo
+      download_url = 'https://github.com/saneax/virtools/tarball/0.1', # I'll explain this in a second
+      keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+      classifiers = [],
+)
